17243029b193a9f6f186068af9e3bb30afa90f34
shashi shanker singh <shankar634@hotmail.com>
2018-03-24 22:56:15 +0530

    News Post section

--- NewsCaptain/Admin/NewsPost.aspx.cs
+++ NewsCaptain/Admin/NewsPost.aspx.cs
@@ -7,11 +7,13 @@ using System.Web.UI.WebControls;
 
 namespace NewsCaptain.Admin
 {
-    public partial class NewsPost : System.Web.UI.Page
+    public partial class NewsPost : common
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack) {
+                PopulateDropDown("Select Name,PublicId from category",NewsCategory,"Name","PublicId");
+            }
         }
     }
 }
